a08e93c29da8190bf6dfe6e8c485d917edece899
kershkov <kershkov@gmail.com>
2021-08-09 10:54:57 +0300

    Update stat-connected.php

---
+++ template-parts/acf-blocks/stat-connected.php
@@ -0,0 +1,34 @@
+<section id="" class="section stay-connected">
+	<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-7 stay-connected__content">
+						<div class="icon-block">
+                            <div class="icon-block__icon">
+							    <img src="<?php echo get_template_directory_uri();?>/images/openhealth/icon1.svg" alt="#">
+                            </div>
+                            <h4 class="h4 icon-block__title">IMAS Vision</h4>
+						</div>
+						<div class="text-block">
+							<ul class="icon-lists__list">
+								<li>Educating, supporting and empowering people living with axSpA by showing them, through the data generated, that they are not alone in their disease.</li>
+                                <li>Ensure that the axSpA patient perspective is integrated into health policy and clinical  decision-making to improve the quality of life and wellbeing of people with axSpA around the world.</li>
+							</ul>
+						</div>
+				</div>
+				<div class="col-12 col-lg-6 icon-lists__col">
+						<div class="icon-block">
+                            <div class="icon-block__icon">
+							    <img src="<?php echo get_template_directory_uri();?>/images/openhealth/icon1.svg" alt="#">
+                            </div>
+                            <h4 class="h4 icon-block__title">IMAS Vision</h4>
+						</div>
+						<div class="text-block">
+							<ul class="icon-lists__list">
+								<li>Educating, supporting and empowering people living with axSpA by showing them, through the data generated, that they are not alone in their disease.</li>
+                                <li>Ensure that the axSpA patient perspective is integrated into health policy and clinical  decision-making to improve the quality of life and wellbeing of people with axSpA around the world.</li>
+							</ul>
+						</div>
+				</div>
+			</div>
+	</div>
+</section>
