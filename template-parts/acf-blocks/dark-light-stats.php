3148c5d6eaf946321aaf9ec1d8093e97a7a10f9a
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-08-10 15:46:02 +0300

    2nd page last section with theme swither coding

---
+++ template-parts/acf-blocks/dark-light-stats.php
@@ -0,0 +1,196 @@
+<section id="switchStats" class="switchStats">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-9">
+                <strong class="switchStats__title">For patients, the most frequent fears and hopes related to axSpA were:</strong>
+                <div class="row">
+                    <div class="col-md-7">
+                        <hr class="switchStats__hr">
+                    </div>
+                    <div class="col-md-5">
+                        <div class="switchStats__switcherGroup">
+                            <a href="#" class="two">
+                                <span>Patient hope</span>
+                                <div class="bg"></div>
+                            </button>
+                            <a href="#" class="one">Patient fears</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="switchStats__listWrapper">
+                    <div class="switchStats__listCarousel">
+                        <ul class="switchStats__list switchStats__list--dark">
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Suffering pain</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 40%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">40</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Loss of mobility</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 35%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">35</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Loss of independence</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 29%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">29.2</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="switchStats__list switchStats__list--light">
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 44%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">44</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                            <li class="switchStats__item row">
+                                <span class="switchStats__label col-auto">Disease progression</span>
+                                <div class="switchStats__activityBarWrapper col">
+                                    <div class="livingAxspa__activityBar switchStats__activityBar">
+                                        <div class="livingAxspa__activityBar--filled" style="width: 76%;"></div>
+                                    </div>
+                                    <span class="livingAxspa__activityValue">76</span>%
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="switchStats__disclaimer">
+                    <strong>Additional fears include:</strong></br>
+                    Financial concerns 1.8%, medication doesn’t work / stops working 1.6%, take care of family / children 1.5%, premature death 1%, paralysis 1%, pass the disease to children 0.9%, anxiety / sadness 0.9%, inability to live with condition / give up 0.6%, sleep interruptions 0.2%.
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<script>
+
+    jQuery(document).ready(function(){
+        jQuery(".switchStats__switcherGroup a").each(function() {
+            $(this).on("click", function(e){
+                e.preventDefault();
+                jQuery(".switchStats").toggleClass('light');
+                $(this).addClass('active');
+                jQuery(".switchStats__listCarousel").toggleClass('light');
+                
+            });
+        });
+    });
+</script>
