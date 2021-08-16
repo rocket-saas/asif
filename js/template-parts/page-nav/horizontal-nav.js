e7c2c45a2c6884d8460ca99462a1096dd2577480
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-08-16 13:08:00 +0300

    JS nav fix

--- js/template-parts/page-nav/horizontal-nav.js
+++ js/template-parts/page-nav/horizontal-nav.js
@@ -1,24 +1,27 @@
 import $ from 'jquery';
 
 function horizontalNav() {
-    $('.axspa-nav--horizontal').each(function(){
-        const navOffset = $('#axspa-nav').offset().top;
-        const navHeight = $('#axspa-nav').height();
-        const navHeightAdmin = navHeight + 32;
-        $(window).scroll(function(){
-            const scrolled = $(this).scrollTop();
-            const headerHeight = $('#header').height() + 'px';           
-            if(scrolled > navOffset) {
-                $('.axspa-nav').css("position","fixed");
-                $('.axspa-nav + section').css("margin-top",navHeight);
-                $('.admin-bar .axspa-nav + section').css("margin-top",navHeight);
-                
-            } else if (scrolled <= navOffset){
-                $('.axspa-nav').css("position","static");
-                $('.axspa-nav + section').css("margin-top",0);
-            }
+    if ($('.axspa-nav--horizontal').length) {
+        $('.axspa-nav--horizontal').each(function(){
+            const navOffset = $('#axspa-nav').offset().top;
+            const navHeight = $('#axspa-nav').height();
+            const navHeightAdmin = navHeight + 32;
+            $(window).scroll(function(){
+                const scrolled = $(this).scrollTop();
+                const headerHeight = $('#header').height() + 'px';           
+                if(scrolled > navOffset) {
+                    $('.axspa-nav').css("position","fixed");
+                    $('.axspa-nav + section').css("margin-top",navHeight);
+                    $('.admin-bar .axspa-nav + section').css("margin-top",navHeight);
+                    
+                } else if (scrolled <= navOffset){
+                    $('.axspa-nav').css("position","static");
+                    $('.axspa-nav + section').css("margin-top",0);
+                }
+            });
         });
-    });
+    }
+    
 }
 
 
