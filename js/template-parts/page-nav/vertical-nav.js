71da02b56e7f8cbcdd4a90f555c71d29a0056a00
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-08-23 19:49:22 +0300

    Vertical nav footer overlap fix

--- js/template-parts/page-nav/vertical-nav.js
+++ js/template-parts/page-nav/vertical-nav.js
@@ -8,19 +8,24 @@ function verticalNav() {
             const navHeight = $('#axspa-nav').height();
             const footerHeight = $('footer').height();
             const navHeightAdmin = navHeight + 32;
+            const headerHeight =  $('header').height()
+            const blocksHeight = $('.blocks').height() - headerHeight - 100;
             console.log(footerHeight);
          
             $(window).scroll(function(){
                 const scrolled = $(this).scrollTop();
                 const headerHeight = $('#header').height() + 'px';
                 const navOffsetBottom = $('#axspa-nav').offset().bottom;
-                console.log(navOffsetBottom);
+                console.log(blocksHeight);
+                console.log(scrolled);
                 if(scrolled > navOffset) {
                     $('.axspa-nav').css("position","fixed");
                     $('.axspa-nav + section').css("margin-top",navHeight);
                     $('.admin-bar .axspa-nav + section').css("margin-top",navHeight);
-                    if (navOffsetBottom < footerHeight) {
-                        $('.axspa-nav').css("top","unset").css("bottom",footerHeight);
+                    if (scrolled > blocksHeight) {
+                        $('.axspa-nav').css("opacity","0")
+                    } else { 
+                        $('.axspa-nav').css("opacity","1")
                     }
                     
                 } else if (scrolled <= navOffset){
