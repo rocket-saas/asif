a7f334a3a3ce41e8bc625ba44fc7875c665cdf6e
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-09-17 15:59:22 +0300

    fuck

--- template-parts/acf-blocks/references.php
+++ template-parts/acf-blocks/references.php
@@ -3,10 +3,12 @@
 if(get_sub_field('references_content')) : ?>
 <section class="imasReferences" id="references">
     <div class="container">
-        <div class="col-lg-9">
-            <div class="references__title"><?php echo get_sub_field('references_title');?></div>
-            <div class="text-block">
-                <?php echo get_sub_field('references_content');?>
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="references__title"><?php echo get_sub_field('references_title');?></div>
+                <div class="text-block">
+                    <?php echo get_sub_field('references_content');?>
+                </div>
             </div>
         </div>
     </div>
