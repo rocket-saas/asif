2b987d06e01dc2565708cf24ea57f9ff5ca212d7
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-09-17 15:51:59 +0300

    Left align references

--- template-parts/acf-blocks/references.php
+++ template-parts/acf-blocks/references.php
@@ -3,7 +3,7 @@
 if(get_sub_field('references_content')) : ?>
 <section class="imasReferences" id="references">
     <div class="container">
-        <div class="col-lg-9 offset-lg-3">
+        <div class="col-lg-9">
             <div class="references__title"><?php echo get_sub_field('references_title');?></div>
             <div class="text-block">
                 <?php echo get_sub_field('references_content');?>
